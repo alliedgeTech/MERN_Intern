78317e73d5a96ba89c882fc04d456a4b43b67dd7
DipPanchal <panchaldip029@gmail.com>
2024-02-28 11:12:22 +0530

    loop

---
+++ JS/loop.js
@@ -0,0 +1,5 @@
+// For loop
+
+for (let i=0;i<=10;i++) {
+  console.log("Alliedge",i)
+}
---
+++ JS/operator.js
@@ -0,0 +1,19 @@
+console.log("welcome to Ternary Ternary")
+
+// Ternary Operator
+
+// let age=25
+// let answer = age>=20 ? "You are aligable for vote" : "You are not aligable for vote";
+// console.log(answer)
+
+// let day= "Saturday"
+// let answer = day=="sunday" || day=="Saturday" ? "Today is holiday" : "Today is not holiday";
+// console.log(answer)
+
+// let age=10;
+// if(age>=20){
+//   console.log("You are aligable for vote")
+// }
+// else{
+//     console.log("You are not aligable for vote")
+// }
