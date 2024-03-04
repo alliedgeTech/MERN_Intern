40222e9f83e997857684c2c0af8e8f5585c11a20
DipPanchal <panchaldip029@gmail.com>
2024-03-04 12:05:10 +0530

    fnx,arrayt

---
+++ JS/array1.js
@@ -0,0 +1,29 @@
+// Array  is Mutable
+// Array is a collection of elements
+
+// let ary = [1,2,3,4,5,6,7,8]
+let ary = [1,"Deep","panchal",21,"ahmedabad"]
+
+// let ary1=[9,10,11,12]
+console.log(ary);
+
+// ary.push(9,10)
+
+// ary.pop()
+// ary.pop()
+// ary.pop()
+
+// let a= ary.toString();
+// console.log(a);
+
+// let ans= ary.concat(ary1)
+// console.log(ans);
+
+// ary.unshift(0,-1,-2,-3,-4,-5,-6,-7,-8)
+// ary.shift()
+// ary.shift()
+
+//----------------slice----------------------
+
+let ans= ary.slice(2,5)
+console.log(ans);
---
+++ JS/function.js
@@ -0,0 +1,26 @@
+function myName() {
+    var x = 45
+    console.log(x);
+    console.log("FirstName:Deep")
+    console.log("LastName:Panchal")
+    console.log("Desg:Founder of Aalliedge Grpup")
+}
+
+myName()
+
+
+function Cal(a, b)//parameter
+{
+    console.log("fnx called!");
+    console.log("sum",a + b);
+    console.log("sub",a - b);
+    console.log("Mul",a * b);
+    console.log("Div",a / b);
+}
+
+//let x= prompt("Enter value a:")
+//let y= prompt("Enter value b:")
+//Cal(parseInt(x),parseInt(y))//argument
+
+Cal(50,10)
+Cal(500,100)
